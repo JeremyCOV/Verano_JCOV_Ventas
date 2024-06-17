592f31d5813fd3dd4ed00a38472d8300dd0d2e21
JeremyCOV <19030310@itesa.edu.mx>
2024-06-16 21:13:27 -0600

    Version03. Controladroes de la API para habilitar su consumo.

--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -1,13 +1,11 @@
-using Blazor.WEB;
-using Blazor.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Sales.WEB;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7072") });
-builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
