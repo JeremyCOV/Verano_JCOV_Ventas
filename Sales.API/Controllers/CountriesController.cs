60e49e013f130c2cfad8ed260bf506cfe8061a7e
JeremyCOV <19030310@itesa.edu.mx>
2024-06-17 12:11:36 -0600

    Version05. Controladores de la API completos.

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -7,7 +7,7 @@ namespace Sales.API.Controllers
 {
     [ApiController]
     [Route("/api/countries")]
-    public class CountriesController: ControllerBase
+    public class CountriesController : ControllerBase
     {
         private readonly DataContext _context;
 
@@ -24,18 +24,45 @@ namespace Sales.API.Controllers
             await _context.SaveChangesAsync();
             return Ok(country);
         }
-       /* public async Task<ActionResult> PostAsync(Country country)
-        {
-            _context.Countries.Add(country);
-            await _context.SaveChangesAsync();
-            return Ok(country);
-        }*/
 
         //GET
         [HttpGet]
         public async Task<IActionResult> GetAsync()
         {
             return Ok(await _context.Countries.ToListAsync());
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetaAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        //PUT
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        //DELETE
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id ==id);
+            if(country == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
