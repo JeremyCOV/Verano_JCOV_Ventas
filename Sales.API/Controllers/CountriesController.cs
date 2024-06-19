07a9e4b2d73df39217689c018f33ecbc9db60bc7
JeremyCOV <19030310@itesa.edu.mx>
2024-06-19 09:45:25 -0600

    Version08. Seed Db Ver1 Terminado.

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -20,9 +20,25 @@ namespace Sales.API.Controllers
         [HttpPost]
         public async Task<ActionResult> PostAsync(Country country)
         {
-            _context.Add(country);
-            await _context.SaveChangesAsync();
-            return Ok(country);
+            try
+            {
+                _context.Add(country);
+                await _context.SaveChangesAsync();
+                return Ok(country);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un pais con el mismo nombre");
+                }
+                return BadRequest("Ya existe un pais con el mismo");
+                //return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception) 
+            {
+                return BadRequest(exception.Message);
+            }
         }
 
         //GET
@@ -46,9 +62,27 @@ namespace Sales.API.Controllers
         [HttpPut]
         public async Task<IActionResult> PutAsync(Country country)
         {
-            _context.Update(country);
-            await _context.SaveChangesAsync();
-            return Ok(country);
+            try
+            {
+                _context.Update(country);
+                await _context.SaveChangesAsync();
+                return Ok(country);
+
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un pais con el mismo nombre");
+                }
+                return BadRequest("Ya existe un pais con el mismo nombre");
+                //return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+            
         }
 
         //DELETE
